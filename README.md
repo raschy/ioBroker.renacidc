b7d94ae2ed4784d576f1111215ff675d17e75f4a
raschy <raschy@gmx.de>
2023-09-05 18:10:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+![Logo](admin/renacidc.png)
+# ioBroker.renacidc
+
+[![NPM version](https://img.shields.io/npm/v/iobroker.renacidc.svg)](https://www.npmjs.com/package/iobroker.renacidc)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.renacidc.svg)](https://www.npmjs.com/package/iobroker.renacidc)
+![Number of Installations](https://iobroker.live/badges/renacidc-installed.svg)
+![Current version in stable repository](https://iobroker.live/badges/renacidc-stable.svg)
+
+[![NPM](https://nodei.co/npm/iobroker.renacidc.png?downloads=true)](https://nodei.co/npm/iobroker.renacidc/)
+
+**Tests:** ![Test and Release](https://github.com/raschy/ioBroker.renacidc/workflows/Test%20and%20Release/badge.svg)
+
+## renacidc adapter for ioBroker
+
+Reading the data from solar inverter
+
+## User manual
+Only the login name and password used for the Renacpower online portal are required for commissioning.
+
+### DISCLAIMER
+
+All product and company names or logos are trademarks™ or registered® trademarks of their respective holders. Use of them does not imply any affiliation with or endorsement by them or any associated subsidiaries! This personal project is maintained in spare time and has no business goal. RENAC is a trademark with Copyright © 2010-2022 China Headquarters Renacpower, Address: Block C-12-1, Comprehensive Bonded Zone, No.1 of Zone 5, No. 20 Datong Road, Suzhou Hi-Tech District, Suzhou.
+
+## Changelog
+<!--
+	Placeholder for the next version (at the beginning of the line):
+	### **WORK IN PROGRESS**
+-->
+
+### **WORK IN PROGRESS**
+* (raschy) initial release
+
+## License
+MIT License
+
+Copyright (c) 2023 raschy <raschy@gmx.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ main.js
@@ -0,0 +1,534 @@
+/*
+ * Created with @iobroker/create-adapter v2.5.0
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+'use strict';
+const utils = require('@iobroker/adapter-core');
+const api = require('./lib/apiClient.js');
+
+// Load your modules here, e.g.:
+// const fs = require("fs");
+
+class Renacidc extends utils.Adapter {
+
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: 'renacidc',
+		});
+		this.on('ready', this.onReady.bind(this));
+		this.on('unload', this.onUnload.bind(this));
+		//
+		this.checkUserDataOk = false;
+		this.executionInterval = 60;
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+		// Initialize your adapter here
+		this.setState('info.connection', { val: false, ack: true });
+		await this.checkUserData();
+		//
+		if (this.checkUserDataOk){
+			await this.requestInverterData();
+			//
+			this.updateInterval = setInterval(async () => {
+				await this.requestInverterData();
+			}, this.executionInterval * 1000);
+		} else {
+			this.setState('info.connection', { val: false, ack: true });
+			this.log.error('Adapter cannot be started without correct settings!');
+		}
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			// Here you must clear all timeouts or intervals that may still be active
+			this.updateInterval && clearInterval(this.updateInterval);
+			// clearTimeout(timeout1);
+			// clearTimeout(timeout2);
+			// ...
+			// clearInterval(interval1);
+			this.log.info('cleaned everything up...');
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	async requestInverterData(){
+		try {
+			const userId = await this.initializeStation();
+			this.log.debug(`UserID: ${userId}`);
+			//
+			const stationIdList = await this.stationList(userId);
+			this.log.debug(`StationList: ${stationIdList}`);
+			for (const stationId of stationIdList) {
+				this.log.debug(`StationID: ${stationId}`);
+				const deviceIdList = await this.deviceList(userId, stationId);
+				this.log.debug(`DeviceIdList: ${deviceIdList}`);
+				for (const inverterSn of deviceIdList) {
+					this.log.debug(`InverterSN: ${inverterSn}`);
+					await this.updateData(await this.inverterData(inverterSn),userId);
+					//await this.alarmList(userId);
+					//await this.inverterDataHistorical(inverterSn);
+				}
+			}
+			//await this.inverterDailyOutput(inverterSn);
+			//await this.readInverterParameters(inverterSn);
+			this.setState('info.connection', { val: true, ack: true });
+		}
+		catch (error) {
+			this.setState('info.connection', { val: false, ack: true });
+			this.log.debug(`[requestInverterData] catch ${JSON.stringify(error)}`);
+		}
+		finally {
+			this.log.debug(`[requestInverterData] finished`);
+		}
+		//
+	}
+
+	/**
+	 * save data in ioBroker datapoints
+	 * @param {*} key
+	 * @param {*} name
+	 * @param {*} value
+	 * @param {*} role
+	 * @param {*} unit
+	 */
+	async persistData(user, key, name, value, role, unit) {
+		const dp_Device = String(user);
+		const path = dp_Device + '.';
+		// Type recognition
+		let type = 'string';
+		if (isNumber(value)) {
+			type = 'number';
+			value = parseFloat(value);
+		}
+		if (typeof value === 'object') {
+			type = 'string';
+			value = JSON.stringify(value);
+		}
+		//console.log(`[persistData] Device "${dp_Device}"  Key "${key}" with value: "${value}" and unit "${unit}" with role "${role}" as type "${type}"`);
+
+		await this.setObjectNotExists(dp_Device, {
+			type: 'channel',
+			common: {
+				name: 'User ID',
+				role: 'info'
+			},
+			native: {}
+		});
+
+		/*
+		await this.setObjectNotExistsAsync(dp_Device, {
+			type: 'device',
+			common: {
+				name: userName
+			},
+			native: {},
+		});
+
+		await this.extendObjectAsync(dp_Device, {
+			common: {
+				name: userName,
+			},
+		});
+		*/
+
+		await this.setObjectNotExistsAsync(path + key, {
+			type: 'state',
+			common: {
+				name: name,
+				role: role,
+				// @ts-ignore
+				type: type,
+				// @ts-ignore
+				unit: unit,
+				read: true,
+				write: false
+			},
+			native: {},
+		});
+
+		await this.setStateAsync(path + key, { val: value, ack: true, q: 0x00 });
+		//
+		function isNumber(n) {
+			return !isNaN(parseFloat(n)) && !isNaN(n - 0);
+		}
+	}
+
+	/**
+	 * prepare data vor ioBroker
+	 * @param {*} data
+	 * @param {number} user
+	 */
+	async updateData(data, user) {
+		if (user < 1) return;
+		//
+		for (const key in data) {
+			if (key != 'none') {
+				const name = makeName(key);
+				const unit = guessUnit(key);
+				await this.persistData(user, key, name, data[key], 'value', unit);
+			}
+		}
+		//
+		// Regex to remove the underscore and process singlewords
+		function makeName(inputString){
+			return inputString.replace(/_/g, ' ').split(' ').map(capitalizeFirstLetter).join(' ');
+		}
+		// Function to convert the first letter of a word into capital letters
+		function capitalizeFirstLetter(word) {
+			return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+		}
+		//	Trying to guess the unit of measurement
+		function guessUnit(inputString){
+			let regex = new RegExp('vol');
+			if (regex.test(inputString)) return 'V';
+			regex = new RegExp('cur');
+			if (regex.test(inputString)) return 'A';
+			regex = new RegExp('fre');
+			if (regex.test(inputString)) return 'Hz';
+			regex = new RegExp('power');
+			if (regex.test(inputString)) return 'W';
+			regex = new RegExp('energy');
+			if (regex.test(inputString)) return 'kWh';
+			regex = new RegExp('capac');
+			if (regex.test(inputString)) return 'AH';
+			regex = new RegExp('temp');
+			if (regex.test(inputString)) return '°C';
+			return ' ';
+		}
+	}
+
+	/**
+	 * readInverterParameters()
+	 * @param {*} inverterSN
+	 * @returns
+	 */
+	async readInverterParameters(inverterSN) {
+		console.log(`[readInverterParameters] InverterSN: ${inverterSN}`);
+		return api.axios
+			.post(
+				'api/setting/loadcodes',
+				{
+					'equSn' : String(inverterSN),
+					'type' : '12'
+				}
+			)
+			.then((response) => {
+				console.log ('readInverterParameters: ', response.data); //####
+				return response.data.data;
+			})
+			.catch((error) => {
+				this.log.warn(`[readInverterParameters] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * inverterDailyOutput()
+	 * @param {*} inverterSN
+	 * @returns
+	 */
+	async inverterDailyOutput(inverterSN) {
+		console.log(`[inverterDailyOutput] InverterSN: ${inverterSN}`);
+		return api.axios
+			.post(
+				'renac/grid/powerChart',
+				{
+					'inv_sn' : String(inverterSN),
+					'time' : '2023-08-30'
+				}
+			)
+			.then((response) => {
+				//console.log ('inverterDailyOutput: ', response); //####
+				return response.data.data;
+			})
+			.catch((error) => {
+				this.log.warn(`[inverterDailyOutput] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * inverterDataHistorical()
+	 * @param {*} inverterSN
+	 * @returns
+	 */
+	async inverterDataHistorical(inverterSN) {
+		console.log(`[inverterDataHistorical] InverterSN: ${inverterSN}`);
+		return api.axios
+			.post(
+				'renac/storage/equChart',			// 2.2.9
+				//'renac/grid/equChart',			// 2.2.10
+				{
+					'inv_sn' : String(inverterSN),
+					'chart_type' : 2,
+					'time' : ' 2023-08-21'
+				}
+			)
+			.then((response) => {
+				console.log ('inverterDataHistorical: ', response.data.data); //####
+				return response.data;
+			})
+			.catch((error) => {
+				this.log.warn(`[inverterDataHistorical] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * alarmList()
+	 * @param {*} userId
+	 * @returns
+	 */
+	async alarmList(userId) {
+		console.log(`[alarmList] UserID: ${userId}`);
+		return api.axios
+			.post(
+				'api/home/errorList2',
+				{
+					'user_id' : userId,
+					'begin_time' : '2023-08-01',
+					'end_time' : '2023-08-31',
+					'offset' : 0,
+					'rows' :50
+				}
+			)
+			.then((response) => {
+				console.log ('alarmList: ', response.data.data); //####
+				/*
+				for (const obj of response.data.data.list) {
+					//this.deviceIdList.push(obj.INV_SN);	// DeviceId's
+				}
+				return response.data.data.list;
+				*/
+			})
+			.catch((error) => {
+				this.log.warn(`[alarmList] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * inverterData()
+	 * @param {*} inverterSN
+	 * @returns data
+	 */
+	async inverterData(inverterSN) {
+		//console.log(`[inverterData] InverterSN: ${inverterSN}`);
+		return api.axios
+			.post(
+				//'renac/grid/equData',		//2.2.5
+				'renac/storage/equData',	//2.2.6
+				{
+					'equ_sn' : String(inverterSN)
+				}
+			)
+			.then((response) => {
+				//console.log ('inverterData: ', response.data.data); //####
+				return response.data.data;
+			})
+			.catch((error) => {
+				this.log.warn(`[inverterData] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * deviceList()
+	 * @param {*} userId
+	 * @param {*} stationId
+	 * @returns
+	 */
+	async deviceList(userId, stationId) {
+		//console.log(`[deviceList] UserID: ${userId} StationID: ${stationId}`);
+		const deviceIdList =[];
+		return api.axios
+			.post(
+				'bg/equList',
+				{
+					'user_id' : userId,
+					'station_id' : stationId,
+					'offset': 0,
+					'rows':10
+				}
+			)
+			.then((response) => {
+				for (const obj of response.data.data.list) {
+					deviceIdList.push(obj.INV_SN);	// DeviceId's
+				}
+				return deviceIdList;
+			})
+			.catch((error) => {
+				this.log.warn(`[deviceList] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * stationStatistic()
+	 * @param {*} stationId
+	 * @returns
+	 */
+	async stationStatistic(stationId) {
+		console.log(`[stationStatistic] StationID: ${stationId}`);
+		return api.axios
+			.post(
+				'renac/station/energy',
+				{
+					'statison_id' : stationId,
+					'time_type' : 4,
+					'time' : '0'
+				}
+			)
+			.then((response) => {
+				console.log ('#stationStatistic: ', response); //####
+				/*
+				for (const obj of response.data.data.list) {
+					this.stationIdList.push(obj.station_id);	// StationId's for devices
+					//console.log ('stationList Type: ', obj.station_type); //####
+				}
+				return response.data.data.list;
+				*/
+			})
+			.catch((error) => {
+				this.log.warn(`[stationStatistic] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * stationList()
+	 * @param {*} userId
+	 * @returns stationIdList[]
+	 */
+	async stationList(userId) {
+		//console.log(`[stationList] UserID: ${userId}`);
+		return api.axios
+			.post(
+				'api/station/list',
+				{
+					'user_id' : userId,
+					'offset': 0,
+					'rows':10
+				}
+			)
+			.then((response) => {
+				const stationIdList = [];
+				for (const obj of response.data.data.list) {
+					stationIdList.push(obj.station_id);
+				}
+				return stationIdList;
+			})
+			.catch((error) => {
+				this.log.warn(`[stationList] error: ${error.code}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * initializeStation()
+	 * @returns userId
+	 */
+	async initializeStation() {
+		//console.log(`[initializeStation]`);
+		return api.axios
+			.post(
+				'api/user/login',
+				{
+					'login_name' : this.config.username,
+					'pwd': this.config.password
+				}
+			)
+			.then((response) => {
+				return response.data.data;
+			})
+			.catch((error) => {
+				this.log.error(`[initializeStation] error: ${error.message}`);
+				return Promise.reject(error);
+			});
+	}
+
+	/**
+	 * checkUserData()
+	 * @returns checkUserDataOk
+	 */
+	async checkUserData() {
+		// The adapters config (in the instance object everything under the attribute "native")
+		// is accessible via this.config:
+		// __________________
+		// Check if credentials are not empty
+		if (!isNonEmptyString(this.config.username)) {
+			this.log.warn('The username you have entered is not text or empty - please check instance configuration.');
+			this.checkUserDataOk = false;
+			return;
+		}
+		//
+		if (!isNonEmptyString(this.config.password)) {
+			this.log.warn('The password you have entered is not text or empty - please check instance configuration.');
+			this.checkUserDataOk = false;
+			return;
+		}
+		// __________________
+		// Check if url is not empty
+		/*
+		if (!isValidInput(this.config.url, 17)) {
+			this.log.warn('The URL you have entered is not text or empty - please check instance configuration.');
+			this.checkUserDataOk = false;
+			return;
+		}
+		if (!this.config.url.endsWith('/')){
+			this.config.url + '/';
+		}
+		*/
+		// __________________
+		// check if the sync time is a number, if not, the string is parsed to a number
+		if (isNaN(this.config.pollInterval) || this.config.pollInterval < 10) {
+			this.executionInterval = 60;
+		} else {
+			this.executionInterval = this.config.pollInterval;
+		}
+		this.log.info(`Retrieving data from the inverter will be done every ${this.executionInterval} seconds`);
+		//
+		this.log.debug(`checkUserData is ready`);
+		this.checkUserDataOk = true;
+		return;
+		//
+		function isNonEmptyString(input) {
+			// Check whether input is a string and whether not empty
+			return typeof input === 'string' && input.trim() !== '';
+		}
+		/*
+		function isValidInput(input, minLength) {
+			// Check whether input is a string and whether the length is greater than or equal to the minimum length.
+			return typeof input === 'string' && input.length >= minLength;
+		}
+		*/
+	}
+
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new Renacidc(options);
+} else {
+	// otherwise start the instance directly
+	new Renacidc();
+}
